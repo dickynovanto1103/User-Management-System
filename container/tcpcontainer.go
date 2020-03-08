97083b6cdb996674e219f894ff202f42eff5ee0c
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-03-08 23:46:19 +0800

    Add Repository for Dependency Injection purpose
    Previously, we put Redis and DB implementation as a global variable
    and it is not good as the code becomes not testable. Now, it should be
    better as the Repository can be injected when TCP server is handling
    the requests

--- container/tcpcontainer.go
+++ container/tcpcontainer.go
@@ -4,57 +4,61 @@ import (
 	"github.com/dickynovanto1103/User-Management-System/internal/repository/dbsql"
 	"github.com/dickynovanto1103/User-Management-System/internal/repository/redis"
 	"github.com/dickynovanto1103/User-Management-System/internal/service/config"
-	pb "github.com/dickynovanto1103/User-Management-System/proto"
-	"google.golang.org/grpc"
 )
 
-var Client pb.UserDataServiceClient
-
-func BuildHttpServerClient(conn *grpc.ClientConn) {
-	Client = pb.NewUserDataServiceClient(conn)
+type Repository struct {
+	DBImpl        *dbsql.DBImpl
+	RedisImpl     *redis.RedisImpl
+	SQLStatements *dbsql.SQLStatements
 }
 
-var DBImpl *dbsql.DBImpl
-var RedisImpl *redis.RedisImpl
+func NewRepository(dbImpl *dbsql.DBImpl, redisImpl *redis.RedisImpl, statements *dbsql.SQLStatements) *Repository {
+	return &Repository{
+		DBImpl:        dbImpl,
+		RedisImpl:     redisImpl,
+		SQLStatements: statements,
+	}
+}
 
-func BuildTCPServerDep() {
+func BuildTCPServerDep() *Repository {
 	configDB := config.LoadConfigDB("config/configDB.json")
 	configRedis := config.LoadConfigRedis("config/configRedis.json")
 
 	redisClient := redis.CreateRedisClient(configRedis)
-	RedisImpl = redis.CreateRedisWrapper(redisClient)
+	redisImpl := redis.CreateRedisWrapper(redisClient)
 
 	dbClient, err := dbsql.PrepareDB(configDB)
 	if err != nil {
 		panic("error creating db client, err:" + err.Error())
 	}
 
-	DBImpl = dbsql.NewDBImpl(dbClient)
+	dbImpl := dbsql.NewDBImpl(dbClient)
 
-	prepareStatements()
+	statements := prepareStatements(dbImpl)
+	return NewRepository(dbImpl, redisImpl, statements)
 }
 
-func prepareStatements() {
-	var err error
-	StatementQueryUser, err = DBImpl.PrepareQueryUser()
+func prepareStatements(dbImpl *dbsql.DBImpl) *dbsql.SQLStatements {
+	statementQueryUser, err := dbImpl.PrepareQueryUser()
 	if err != nil {
 		panic("error preparing statement query user, err:" + err.Error())
 	}
 
-	StatementQueryPassword, err = DBImpl.PrepareQueryPassword()
+	statementQueryPassword, err := dbImpl.PrepareQueryPassword()
 	if err != nil {
 		panic("error preparing statement query password, err:" + err.Error())
 	}
 
-	StatementUpdateNickname, err = DBImpl.PrepareUpdateNickname()
+	statementUpdateNickname, err := dbImpl.PrepareUpdateNickname()
 	if err != nil {
 		panic("error preparing statement update nickname, err:" + err.Error())
 	}
 
-	StatementUpdateProfile, err = DBImpl.PrepareUpdateProfile()
+	statementUpdateProfile, err := dbImpl.PrepareUpdateProfile()
 	if err != nil {
 		panic("error preparing statement update profile, err:" + err.Error())
 	}
 
-	sqlStatements := dbsql.NewSQLStatements(StatementQueryUser, StatementQueryPassword, StatementUpdateNickname, StatementUpdateProfile)
+	sqlStatements := dbsql.NewSQLStatements(statementQueryUser, statementQueryPassword, statementUpdateNickname, statementUpdateProfile)
+	return sqlStatements
 }
