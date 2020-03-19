8260425445d97a89261addd214d421ca8d323b16
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-03-20 00:24:55 +0800

    Add unit test for getUserDataFromCache
    Add unit test for full test coverage for this method, i.e. handling
    additional redis errors

--- internal/service/tcpserver/requesthandler/helper_test.go
+++ internal/service/tcpserver/requesthandler/helper_test.go
@@ -1,6 +1,7 @@
 package requesthandler
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/dickynovanto1103/User-Management-System/internal/model"
@@ -10,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetUserDataFromCache(t *testing.T) {
+func TestGetUserDataFromCacheNormal(t *testing.T) {
 	mockCtrl, mockRedis := getRedisNormal(t)
 	defer mockCtrl.Finish()
 
@@ -22,7 +23,33 @@ func TestGetUserDataFromCache(t *testing.T) {
 		ProfileURL: "default",
 	}
 	assert.Equal(t, user, expectedUser)
+}
 
+func TestGetUserDataFromCacheErrorUsername(t *testing.T) {
+	mockCtrl, mockRedis := getRedisErrorGetUsername(t)
+	defer mockCtrl.Finish()
+
+	user, err := getUserDataFromCache("user1", mockRedis)
+	assert.Equal(t, user, model.User{})
+	assert.Equal(t, err, fmt.Errorf("error getting key user1username"))
+}
+
+func TestGetUserDataFromCacheErrorNickname(t *testing.T) {
+	mockCtrl, mockRedis := getRedisErrorGetNickname(t)
+	defer mockCtrl.Finish()
+
+	user, err := getUserDataFromCache("user1", mockRedis)
+	assert.Equal(t, user, model.User{})
+	assert.Equal(t, err, fmt.Errorf("error getting key user1nickname"))
+}
+
+func TestGetUserDataFromCacheErrorProfile(t *testing.T) {
+	mockCtrl, mockRedis := getRedisErrorGetProfile(t)
+	defer mockCtrl.Finish()
+
+	user, err := getUserDataFromCache("user1", mockRedis)
+	assert.Equal(t, user, model.User{})
+	assert.Equal(t, err, fmt.Errorf("error getting key user1profile"))
 }
 
 func getRedisNormal(t *testing.T) (*gomock.Controller, redis.Redis) {
@@ -35,5 +62,42 @@ func getRedisNormal(t *testing.T) (*gomock.Controller, redis.Redis) {
 	mockRedis.EXPECT().Get(key).Return("user1", nil)
 	key = "user1profile"
 	mockRedis.EXPECT().Get(key).Return("default", nil)
+
+	return mockCtrl, mockRedis
+}
+
+func getRedisErrorGetUsername(t *testing.T) (*gomock.Controller, redis.Redis) {
+	mockCtrl := gomock.NewController(t)
+	mockRedis := redis.NewMockRedis(mockCtrl)
+
+	key := "user1username"
+	mockRedis.EXPECT().Get(key).Return("", fmt.Errorf("error getting key %v", key))
+
+	return mockCtrl, mockRedis
+}
+
+func getRedisErrorGetNickname(t *testing.T) (*gomock.Controller, redis.Redis) {
+	mockCtrl := gomock.NewController(t)
+	mockRedis := redis.NewMockRedis(mockCtrl)
+
+	key := "user1username"
+	mockRedis.EXPECT().Get(key).Return("", nil)
+	key = "user1nickname"
+	mockRedis.EXPECT().Get(key).Return("", fmt.Errorf("error getting key %v", key))
+
+	return mockCtrl, mockRedis
+}
+
+func getRedisErrorGetProfile(t *testing.T) (*gomock.Controller, redis.Redis) {
+	mockCtrl := gomock.NewController(t)
+	mockRedis := redis.NewMockRedis(mockCtrl)
+
+	key := "user1username"
+	mockRedis.EXPECT().Get(key).Return("", nil)
+	key = "user1nickname"
+	mockRedis.EXPECT().Get(key).Return("", nil)
+	key = "user1profile"
+	mockRedis.EXPECT().Get(key).Return("", fmt.Errorf("error getting key %v", key))
+
 	return mockCtrl, mockRedis
 }
