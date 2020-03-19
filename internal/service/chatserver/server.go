ca65864415a2bc1f3b8eab47954a9a696c30367f
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-03-19 23:43:22 +0800

    Add Message struct and its construction

--- internal/service/chatserver/server.go
+++ internal/service/chatserver/server.go
@@ -1,12 +1,28 @@
 package chatserver
 
+import "fmt"
+
 type Server struct{}
 
 func NewServer() *Server {
 	return &Server{}
 }
 
+type Message struct {
+	msg      string
+	receiver int
+}
+
+func NewMessage(msg string, receiver int) *Message {
+	return &Message{
+		msg:      msg,
+		receiver: receiver,
+	}
+}
+
 func (s *Server) SendMessage(receiverId int, message string) {
 	//create struct message
+	msg := NewMessage(message, receiverId)
 	//put message inside kafka
+	fmt.Println("msg:", msg)
 }
