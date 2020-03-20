2634f0e153054265b59522b322b4d3f1babe5f52
dickynovanto1103 <dickynovanto1103@gmail.com>
2020-03-20 18:08:27 +0800

    Add getUserData with cache success unit test

--- internal/service/tcpserver/requesthandler/helper_test.go
+++ internal/service/tcpserver/requesthandler/helper_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/dickynovanto1103/User-Management-System/internal/repository/dbsql"
+
 	"github.com/dickynovanto1103/User-Management-System/internal/model"
 
 	"github.com/dickynovanto1103/User-Management-System/internal/repository/redis"
@@ -100,4 +102,43 @@ func getRedisErrorGetProfile(t *testing.T) (*gomock.Controller, redis.Redis) {
 	mockRedis.EXPECT().Get(key).Return("", fmt.Errorf("error getting key %v", key))
 
 	return mockCtrl, mockRedis
+}
+
+func TestGetUserDataCacheSuccessful(t *testing.T) {
+	mockRedisCtrl, mockRedis := getRedisNormal(t)
+	mockDBCtrl, mockDB := getDBMock(t)
+	defer mockRedisCtrl.Finish()
+	defer mockDBCtrl.Finish()
+
+	username := "user1"
+	user, err := getUserData(username, mockRedis, mockDB)
+	expectedUser := model.User{
+		Username:   username,
+		Nickname:   username,
+		ProfileURL: "default",
+	}
+	assert.Equal(t, user, expectedUser)
+	assert.Equal(t, err, nil)
+}
+
+func getDBMock(t *testing.T) (*gomock.Controller, dbsql.DB) {
+	mockCtrl := gomock.NewController(t)
+	dbMock := dbsql.NewMockDB(mockCtrl)
+
+	return mockCtrl, dbMock
+}
+
+func getDBMockNormal(t *testing.T) (*gomock.Controller, dbsql.DB) {
+	mockCtrl := gomock.NewController(t)
+	dbMock := dbsql.NewMockDB(mockCtrl)
+
+	username := "user1"
+	user := model.User{
+		Username:   username,
+		Nickname:   username,
+		ProfileURL: "default",
+	}
+	dbMock.EXPECT().GetUser(username).Return(user, nil)
+
+	return mockCtrl, dbMock
 }
